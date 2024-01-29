2e244e84210da428fc29555b759a7d41a3987a4c
Enzhy58 <ang.borisova2015@yandex.ru>
2024-01-29 11:47:52 +0300

    added teleport

--- js/snake.js
+++ js/snake.js
@@ -24,8 +24,30 @@ const moveSnake = () => {
         newMovementSnake = { x: headSnake.x, y: headSnake.y + 1, d: direction, h: true }
     }
 
+    newMovementSnake = _setTeleportSnake(state.snake, newMovementSnake);
+
     state.snake.tail.shift();
     headSnake.h = false;
 
     state.snake.tail.push(newMovementSnake);
+};
+
+const _setTeleportSnake = (snake, newHeadSnake) => {
+    const { direction } = snake;
+    const rowEdge = row - 1;
+
+    if(newHeadSnake.x > rowEdge && direction === "right"){
+        return { x: 0, y: newHeadSnake.y, d: newHeadSnake.d, h: newHeadSnake.h };
+    }
+    if(newHeadSnake.x < 0 && direction === "left"){
+        return { x: rowEdge, y: newHeadSnake.y, d: newHeadSnake.d, h: newHeadSnake.h };
+    }
+    if(newHeadSnake.y < 0 && direction === "up"){
+        return { x: newHeadSnake.x, y: rowEdge, d: newHeadSnake.d, h: newHeadSnake.h };
+    }
+    if(newHeadSnake.y > rowEdge && direction === "down"){
+        return { x: newHeadSnake.x, y: 0, d: newHeadSnake.d, h: newHeadSnake.h };
+    }
+
+    return { x: newHeadSnake.x, y: newHeadSnake.y, d: newHeadSnake.d, h: newHeadSnake.h };
 };
